c36b99048bbfb21c548534fe582924d69f41d97f
Xizhe741 <fengxz24@mails.tsinghua.edu.cn>
2026-04-23 15:20:54 +0800

    feat: linear and cosine noise schedules

---
+++ src/diffusion/schedule.py
@@ -0,0 +1,25 @@
+"""
+Noise schedule for forward process. Contains:
+
+1. linear_schedule
+2. cosine_schedule
+
+"""
+
+import math
+import torch
+
+def linear_schedule(T,beta_0 = 1e-4,beta_1 = 0.02):
+    beta = torch.linspace(beta_0, beta_1, T)
+    alpha = 1 - beta 
+    alpha_bar = torch.cumprod(alpha, dim=0)
+    return beta,alpha,alpha_bar
+
+def cosine_schedule(T):
+    s = 8e-3
+    steps = torch.arange(T + 1, dtype=torch.float64)
+    f = torch.cos(((steps / T) + s) / (1 + s) * (math.pi / 2)) ** 2
+    alpha_bar = f/f[0]
+    alpha = alpha_bar[1:] / alpha_bar[:-1] 
+    beta  = (1 - alpha).clamp(max = 0.999)
+    return beta.float(),alpha.float(),alpha_bar[1:].float()
